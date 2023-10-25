5dc1914c3f8b499d523b2e2a42a450fdaf05a11b
jzh <1587315093@qq.com>
2023-10-25 15:47:48 +0800

    chore: deploy

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'dumi';
 
-const path = '/scratch-card';
+const projectPath = '/scratch-card/';
+const projectName = 'ScratchCard';
 
 export default defineConfig({
-  base: path,
-  publicPath: path,
+  base: projectPath,
+  publicPath: projectPath,
   outputPath: 'docs-dist',
-  title: 'ScratchCard',
+  title: projectName,
   themeConfig: {
-    name: 'ScratchCard',
+    name: projectName,
     nav: [
       { title: '介绍', link: '/guide' },
-      { title: '组件', link: '/components/scratch-card' }, // components会默认自动对应到src文件夹
+      { title: '组件', link: '/components/scratch-card' },
     ],
   },
   styles: [
